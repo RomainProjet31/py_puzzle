cf1d0f71f0e38dc2a8632be673c28cd8b7b2e59f
RomainProjet31 <romain.giroux@outlook.fr>
2024-05-29 19:31:45 +0200

    Implement a tutorial to be more user friendly

--- src/ui/help.py
+++ src/ui/help.py
@@ -1,17 +1,41 @@
 import pygame
 
+from src.ui.text_manager import TEXT_MANAGER
+
 
 class Help:
     """
     Class to help the player to know better about the game
     """
 
+    _directional = "Use the directional keys to move"
+    _move_lock = "Once you moved, only an obstacle can stop you"
+    _goal = "The chests are the goals. Reach them all to end the level"
+    _special_blocks = "Do not trust the walls ;)"
+    _image_path = "./assets/help/tutorial.png"
+
     def __init__(self, screen_size: tuple) -> None:
+        self.texts_to_render = [
+            self._directional,
+            self._move_lock,
+            self._goal,
+            self._special_blocks,
+        ]
         self.display = False
         self.bg = pygame.rect.Rect(0, 0, screen_size[0], screen_size[1])
         self.font = pygame.font.SysFont(None, 48)
-        self.txt = self.font.render("TEST", True, (255, 255, 255))
+        self.tutorial_img = pygame.image.load(self._image_path).convert_alpha()
+        self.scaled = False
 
     def render(self, surface: pygame.Surface) -> None:
         pygame.draw.rect(surface, (0, 0, 0), self.bg)
-        surface.blit(self.txt, (10, 10))
+        curr_y = TEXT_MANAGER.render_texts(
+            self.texts_to_render, surface, pygame.Vector2(10, 10), size=30
+        )
+        if not self.scaled:
+            self.tutorial_img = pygame.transform.scale(
+                self.tutorial_img,
+                (surface.get_width() - 20, surface.get_height() - curr_y - 10),
+            )
+            self.scaled = True
+        surface.blit(self.tutorial_img, (10, curr_y))
--- src/ui/text_manager.py
+++ src/ui/text_manager.py
@@ -9,8 +9,14 @@ class _TextManager:
     def __init__(self) -> None:
         self.font = pygame.font.SysFont(None, 48)
 
-    def render(self, surface: Surface, text: str, pos: Vector2, color=_white) -> None:
-        surface.blit(self.font.render(text, True, color), pos)
+    def render(
+        self, surface: Surface, text: str, pos: Vector2, color=_white, size: int = 48
+    ) -> None:
+        if size != 48 and size > 0:
+            font = pygame.font.SysFont(None, size)
+        else:
+            font = self.font
+        surface.blit(font.render(text, True, color), pos)
 
     def size(self, text: str) -> tuple:
         return self.font.size(text)
@@ -34,6 +40,24 @@ class _TextManager:
         pos_txt = Vector2(x, y)
         self.render(surface, text, pos_txt, color)
         return pos_txt
+
+    def render_texts(
+        self,
+        lst_text: list[str],
+        surface: Surface,
+        base_pos: Vector2,
+        color=_white,
+        size: int = 48,
+    ) -> int:
+        """
+        :return The current y coordinate
+        """
+        offset_y = 5
+        current_pos = base_pos
+        for txt in lst_text:
+            self.render(surface, txt, current_pos, color, size)
+            current_pos.y += self.size(txt)[1] + offset_y
+        return current_pos.y
 
 
 TEXT_MANAGER = _TextManager()
